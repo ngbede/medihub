c916c26201b939766807c7a67a347a4e15138818
Emmanuel <ogasule001@gmail.com>
2021-03-23 16:06:34 +0100

    show user appointments & medical history

--- lib/screens/appointments.dart
+++ lib/screens/appointments.dart
@@ -2,20 +2,101 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:medihub/constants.dart';
+import 'package:medihub/widgets/styletext.dart';
 
 class Appointments extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0XFFDCF0FE),
+      appBar: AppBar(
+        title: Text("Medical Appointments"),
+        backgroundColor: Color(0XFF35D4C0),
+        automaticallyImplyLeading: false,
+      ),
       body: Column(
         children: <Widget>[
           StreamBuilder<QuerySnapshot>(
             stream: store.collection("appointments").snapshots(),
             builder: (context, snapshot) {
-              List<Card> appointmentStreams = [];
+              List<Padding> appointmentStreams = [];
               if (snapshot.hasData) {
                 final doclist = snapshot.data.docs;
-                for (var userdata in doclist) {}
+                for (var userdata in doclist) {
+                  if (userdata.data()["patient"] == auth.currentUser.uid) {
+                    final String docName = userdata.data()["doctorName"];
+                    final String hospitalName = userdata.data()["hospitalName"];
+                    final String location = userdata.data()["location"];
+                    final date = userdata.data()["date"];
+                    //final state = userdata.data()["State"];
+                    final String room = userdata.data()["room"];
+                    final String time = userdata.data()["time"];
+                    final String ward = userdata.data()["ward"];
+                    appointmentStreams.add(
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Card(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              children: [
+                                Align(
+                                  alignment: Alignment.topLeft,
+                                  child: StyledCard("Doctor", docName),
+                                ),
+                                Align(
+                                  alignment: Alignment.topLeft,
+                                  child: StyledCard("Hospital", hospitalName),
+                                ),
+                                Align(
+                                  alignment: Alignment.topLeft,
+                                  child: StyledCard("Location", location),
+                                ),
+                                Align(
+                                  alignment: Alignment.topLeft,
+                                  child: StyledCard("Ward", ward),
+                                ),
+                                Align(
+                                  alignment: Alignment.topLeft,
+                                  child: StyledCard("Room", room),
+                                ),
+                                Align(
+                                  alignment: Alignment.topLeft,
+                                  child: StyledCard("Date", date),
+                                ),
+                                Align(
+                                  alignment: Alignment.topLeft,
+                                  child: StyledCard("Time", time),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  }
+                }
+                if (appointmentStreams.isEmpty) {
+                  return Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "You have no medical appointments yet",
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold),
+                          ),
+                          Icon(
+                            FluentIcons.cloud_20_regular,
+                            size: 30,
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                }
               } else {
                 return Center(
                   child: Column(
@@ -29,6 +110,11 @@ class Appointments extends StatelessWidget {
                   ),
                 );
               }
+              return Expanded(
+                child: ListView(
+                  children: appointmentStreams,
+                ),
+              );
             },
           ),
         ],
--- lib/screens/diaghistory.dart
+++ lib/screens/diaghistory.dart
@@ -1,7 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:medihub/config/textstyles.dart';
 import 'package:medihub/constants.dart';
+import 'package:medihub/widgets/aligninfo.dart';
+import 'package:medihub/widgets/styletext.dart';
 
 class History extends StatelessWidget {
   @override
@@ -39,31 +42,91 @@ class History extends StatelessWidget {
                               child: Column(
                                 children: [
                                   Align(
-                                      alignment: Alignment.topLeft,
-                                      child:
-                                          Text("Patient name: $patientName")),
+                                    alignment: Alignment.topLeft,
+                                    child: StyledCard("Name", patientName),
+                                  ),
                                   Align(
-                                      alignment: Alignment.topLeft,
-                                      child: Text("Gender: $gender")),
+                                    alignment: Alignment.topLeft,
+                                    child: StyledCard("Gender", gender),
+                                  ),
                                   Align(
-                                      alignment: Alignment.topLeft,
-                                      child:
-                                          Text("Year of Birth: $yearOfBirth")),
+                                    alignment: Alignment.topLeft,
+                                    child: StyledCard(
+                                        "Year of Birth", "$yearOfBirth"),
+                                  ),
                                   Divider(),
+                                  AlignInfo(
+                                    name: "Sickness Name",
+                                    infoStyle: confirmStyleHeading,
+                                    pad: 15.0,
+                                  ),
                                   Align(
-                                      alignment: Alignment.topLeft,
-                                      child:
-                                          Text("Sickness Name: $sicknessName")),
-                                  Align(
-                                      alignment: Alignment.topLeft,
-                                      child: Text("Description: $description")),
-                                  Align(
-                                      alignment: Alignment.topLeft,
+                                    alignment: Alignment.topLeft,
+                                    child: Padding(
+                                      padding: EdgeInsets.only(
+                                          left: 15, top: 5, bottom: 5),
                                       child: Text(
-                                          "Confidence level: $percentage")),
+                                        "$sicknessName",
+                                        style: TextStyle(
+                                            letterSpacing: 2.0,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ),
+                                  ),
+                                  AlignInfo(
+                                    name: "Description",
+                                    infoStyle: confirmStyleHeading,
+                                    pad: 15.0,
+                                  ),
                                   Align(
-                                      alignment: Alignment.topLeft,
-                                      child: Text("Symptoms: $symptoms")),
+                                    alignment: Alignment.topLeft,
+                                    child: Padding(
+                                      padding: EdgeInsets.only(
+                                          left: 15, top: 5, bottom: 5),
+                                      child: Text(
+                                        "$description",
+                                        style: TextStyle(
+                                            letterSpacing: 2.0,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ),
+                                  ),
+                                  AlignInfo(
+                                    name: "Confidence Level",
+                                    infoStyle: confirmStyleHeading,
+                                    pad: 15.0,
+                                  ),
+                                  Align(
+                                    alignment: Alignment.topLeft,
+                                    child: Padding(
+                                      padding: EdgeInsets.only(
+                                          left: 15, top: 5, bottom: 5),
+                                      child: Text(
+                                        "$percentage",
+                                        style: TextStyle(
+                                            letterSpacing: 2.0,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ),
+                                  ),
+                                  AlignInfo(
+                                    name: "Symptoms",
+                                    infoStyle: confirmStyleHeading,
+                                    pad: 15.0,
+                                  ),
+                                  Align(
+                                    alignment: Alignment.topLeft,
+                                    child: Padding(
+                                      padding: EdgeInsets.only(
+                                          left: 15, top: 5, bottom: 5),
+                                      child: Text(
+                                        "$symptoms",
+                                        style: TextStyle(
+                                            letterSpacing: 2.0,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ),
+                                  ),
                                 ],
                               ),
                             ),
@@ -72,12 +135,33 @@ class History extends StatelessWidget {
                       );
                     }
                   }
+                  if (historyStreams.isEmpty) {
+                    return Center(
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "You have no medical diagnosis history",
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                            Icon(
+                              FluentIcons.emoji_sad_24_regular,
+                              size: 30,
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  }
                 } else {
                   return Center(
                     child: Column(
                       children: [
                         Text(
-                          "No Medical History To show now",
+                          "No Diagnosis History To show for now",
                         ),
                         Icon(
                           FluentIcons.emoji_sad_24_regular,
